6834342458f9ea7714ec274698a5af1689bc4ce6
uS-aito <h2so40627@gmail.com>
2016-08-28 04:18:34 +0900

    パスワード管理用クラスに修正、ログインして金額取得まで成功(OTPが問題)

--- aes_cipher.py
+++ aes_cipher.py
@@ -7,35 +7,46 @@ import sys
 
 
 class AESCipher(object):
-    def __init__(self, key, block_size=32):
-        self.key = hashlib.md5(key).hexdigest()
-        self.bs = block_size
+	def __init__(self, key, block_size=32):
+		self.key = hashlib.md5(key).hexdigest()
+		self.bs = block_size
 
-    def __encrypt(self, raw):
-        raw = self._pad(raw)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return base64.b64encode(iv + cipher.encrypt(raw))
+	def __encrypt(self, raw):
+		raw = self.__pad(raw)
+		iv = Random.new().read(AES.block_size)
+		cipher = AES.new(self.key, AES.MODE_CBC, iv)
+		return base64.b64encode(iv + cipher.encrypt(raw))
 
-    def __decrypt(self, enc):
-        enc = base64.b64decode(enc)
-        iv = enc[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size:]))
+	def __decrypt(self, enc):
+		enc = base64.b64decode(enc)
+		iv = enc[:AES.block_size]
+		cipher = AES.new(self.key, AES.MODE_CBC, iv)
+		return self.__unpad(cipher.decrypt(enc[AES.block_size:]))
 
-    def __pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+	def __pad(self, s):
+		return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
 
-    def __unpad(self, s):
-        return s[:-ord(s[len(s)-1:])]
-    
-    def write_pass(self):
-        sys.stdout.write("Master ")
-        cipher = AESCipher(getpass())
-        sys.stdout.write("ID: ")
-        id = raw_input()
-        ciphered = cipher.encrypt(getpass())
+	def __unpad(self, s):
+		return s[:-ord(s[len(s)-1:])]
+	
+	def write_pass(self):
+		sys.stdout.write("ID: ")
+		id = self.__encrypt(raw_input())
+		ps = self.__encrypt(getpass())
+		with open("id-pass.txt","w") as wfp:
+			wfp.write(id+"\n")
+			wfp.write(ps+"\n")
+
+	def read_pass(self):
+		with open("id-pass.txt","r") as rfp:
+			id = self.__decrypt(rfp.readline())
+			ps = self.__decrypt(rfp.readline())
+		return (id,ps)
 
 if __name__ == "__main__":
-    print id
-    print ciphered
+	sys.stdout.write("Master ")
+	aesc = AESCipher(getpass())
+	aesc.write_pass()
+	id, ps = aesc.read_pass()
+	print id
+	print ps
--- lushan.py
+++ lushan.py
@@ -3,26 +3,52 @@
 import os   
 import time
 from selenium import webdriver
-import aes_cipher
+from aes_cipher import AESCipher
+from getpass import getpass
 
 class aeon_card_scraping:
 	url = "http://www.aeon.co.jp/creditcard/"
 	DRIVER_PATH = os.path.join(os.path.dirname(__file__),"chromedriver")
-	cipher = aes_cipher("test")
 
 	def start(self):
+		# read id-pass file
+		cipher = AESCipher(getpass())
+		try:
+			id,ps = cipher.read_pass()
+		except:
+			print "Cannot read id-pass.txt"
+			exit()
+
 		browser = webdriver.Chrome(self.DRIVER_PATH)
 		browser.get(self.url)
 		try:
 			# top page block
-			login_page_button = browser.find_element_by_css_selector("a.btn-03.str-03.str-cv")
-			login_page_button.click()
+			browser.find_element_by_css_selector("a.btn-03.str-03.str-cv").click()
 			
 			# login page block
+			browser.find_element_by_name("netMemberId").send_keys(id)
+			browser.find_element_by_name("password").send_keys(ps)
+			browser.find_element_by_css_selector("button.btn-03.str-03.center").click()
 
+			# needs otp process
+
+			# displaying charge page block
+			print browser.find_element_by_css_selector("span.num").text
+
+			# for debug(pause browser)
+			raw_input()
 		finally:
 			browser.quit()
 
+			"""
+			ワンタイムPWを要求する画面
+
+			ワンタイムPWを入力する画面
+			browser.find_element_by_name("otpwd").send_keys(id)
+			terminalRegist2
+
+			端末の登録をする画面
+			"""
 
 if __name__ == "__main__":
 	acs = aeon_card_scraping();
